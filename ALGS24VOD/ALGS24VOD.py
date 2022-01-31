f4aaf35c597c823e4d365f0db2c8b72ab86bd4df
PenutChen <penut85420@gmail.com>
2022-01-31 21:01:54 +0800

    Add timer broadcasting message

--- ALGS24VOD/ALGS24VOD.py
+++ ALGS24VOD/ALGS24VOD.py
@@ -10,7 +10,9 @@ from ALGS24VOD import CheckVOD, load_json, timestamp
 
 class ALGS24VOD(commands.Bot):
     def __init__(self):
+        self._title = None
         self.title = None
+        self.channel = None
         self.config = load_json("Config.json")
         super().__init__(
             token=self.config["access_token"],
@@ -20,20 +22,26 @@ class ALGS24VOD(commands.Bot):
 
     async def event_ready(self):
         print(f"Ready | {self.nick}")
-        while len(self.connected_channels) < 1:
-            await self.join_channels([self.config["broadcaster_id"]])
-        self.channel = self.connected_channels[0]
-        await asyncio.create_task(self.monitor_title())
+
+        tasks = [self.monitor_title, self.broadcast_title]
+        tasks = [asyncio.create_task(t()) for t in tasks]
+        for t in tasks:
+            await t
 
     async def monitor_title(self):
         while True:
-            self.config = load_json("Config.json")
-            title = CheckVOD().check_dir(self.config["target_dir"])
-            if title != self.title:
-                self.title = title
-                self.set_channel_title(self.config["title_fmt"] % title)
-                await self.channel.send(f"現正播放「{title}」")
-                await asyncio.sleep(self.config["timer"])
+            await self.refresh_config()
+            if self._title != self.title:
+                self.title = self._title
+                self.set_channel_title(self.config["title_fmt"] % self.title)
+                await self.channel.send(self.config["message_fmt"] % self.title)
+            await asyncio.sleep(self.config["timer"])
+
+    async def broadcast_title(self):
+        while True:
+            await asyncio.sleep(self.config["message_timer"])
+            await self.refresh_config()
+            await self.channel.send(self.config["message_fmt"] % self._title)
 
     def set_channel_title(self, title):
         url = f"http://localhost:8080/title?title={title}"
@@ -54,6 +62,13 @@ class ALGS24VOD(commands.Bot):
 
         resp = requests.get(url, cookies=self.config["cookie"])
         print(f'{timestamp()} [{resp.status_code}] Set Title "{title}"')
+
+    async def refresh_config(self):
+        self.config = load_json("Config.json")
+        while self.channel is None:
+            await self.join_channels([self.config["broadcaster_id"]])
+            self.channel = self.get_channel(self.config["broadcaster_id"])
+        self._title = CheckVOD().check_dir(self.config["target_dir"])
 
 
 def main():
--- README.md
+++ README.md
@@ -10,7 +10,9 @@
 + `target_dir` 是放置 VOD 的目錄路徑，路徑中的反斜線必須重複兩次
 + `broadcaster_id` 是想要修改標題的頻道 ID
 + `timer` 是指程式多久檢查一次 PotPlayer 的播放檔案，單位為秒
++ `message_timer` 在聊天室推播訊息的時間間隔，單位為秒
 + `title_fmt` 是標題格式，使用 `%s` 代表檔案名稱要擺的位置
++ `message_fmt` 要推播到聊天室的訊息格式，使用 `%s` 代表檔案名稱要擺的位置
 
 ## 授權
 本軟體使用 MIT 授權條款
